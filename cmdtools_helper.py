db5179390e2e82d8918fc8ce1fd55b1719bb4b6f
HugeBrain16 <hugebrain16@gmail.com>
2023-05-20 15:44:12 +0700

    add `exec_group()` function

--- cmdtools_helper.py
+++ cmdtools_helper.py
@@ -1,3 +1,17 @@
 import cmdtools
+from cmdtools.ext.command import Group
 
 __version__ = "0.0.1"
+
+
+def exec_group(group: Group, command: cmdtools.Cmd, attrs: dict | None = None):
+    if attrs is None:
+        attrs = {}
+
+    for cmd in group.commands:
+        if command.name == cmd.name or command.name in cmd.aliases:
+            executor = cmdtools.Executor(cmd, cmd.callback, attrs=attrs)
+
+            return executor.exec()
+
+    raise cmdtools.NotFoundError(f"Command not found: {command.name}", command.name)
